f7e3b0a6d3cc13eb671b41fe3012bbbcc46ad0c0
Jonnits <jonathanrubra@gmail.com>
2025-01-28 11:33:49 -0800

    Created Express GETs for 2 endpoints
    - One at "/movies" that returns a JSON list of data on my top 10 movies
    - One at "/" that returns the text response "What movies move you?"

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require("express");
+
 const app = express();
+
+app.get('/movies', (req, res) => {
+    const topMovies = [
+{ title: 'Movie 1', year: 2024, genre: 'Action' },
+{ title: 'Movie 2', year: 2023, genre: 'Drama' },
+{ title: 'Movie 3', year: 2022, genre: 'Comedy' },
+{ title: 'Movie 4', year: 2021, genre: 'Documentary' },
+{ title: 'Movie 5', year: 2020, genre: 'Horror' },
+{ title: 'Movie 6', year: 2019, genre: 'Thriller' },
+{ title: 'Movie 7', year: 2018, genre: 'Crime' },
+{ title: 'Movie 8', year: 2017, genre: 'Science Fiction' },
+{ title: 'Movie 9', year: 2016, genre: 'Western' },
+{ title: 'Movie 10', year: 2015, genre: 'Romance' },
+    ];
+res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+    res.send('Which movies move you?');
+});
+
+app.use(express.static('public'));
