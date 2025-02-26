8d5ca27b2975675970d6212dc721a13b8e71b651
Jonnits <jonathanrubra@gmail.com>
2025-02-25 16:06:38 -0800

    Connected Heroku and MongoDB

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://127.0.0.1:27017/test")
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+// mongoose.connect("mongodb://127.0.0.1:27017/test")
+//  .then(() => console.log('Connected to MongoDB'))
+//  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// mongoose.connect(process.env.CONNECTION_URI, { use NewUrlParser: true, useUnifiedTopology: true});
+//  .then(() => console.log('Connected to MongoDB'))
+//  .catch(err => console.error('Could not connect to MongoDB', err));
 
 const app = express();
 app.use(morgan('dev'));
